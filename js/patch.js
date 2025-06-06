f484383ff48587203966558c61eef08b73f9d738
sonofluiz <84770635+sonofluiz@users.noreply.github.com>
2025-06-06 16:44:52 -0500

    Update patch.js
    changed dB decrease from -15 to -20dB

--- js/patch.js
+++ js/patch.js
@@ -25700,7 +25700,7 @@ let osc = new Tone.Oscillator
 osc.volume.value = inVolume.get();
 
 let gainNode = audioCtx.createGain();
-gainNode.gain.value = -15;
+gainNode.gain.value = -20;
 //const fadeInTime = .001;  // ✅ Define fade-in time
 //const fadeOutTime = .001; // ✅ Define fade-out time
 osc.connect(gainNode);
